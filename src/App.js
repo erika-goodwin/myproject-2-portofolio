d9e5a7663f1d3cc11457ef5d5a1953653270f6d4
Erika Hashizume <erikahashizume@Erikas-MacBook-Pro.local>
2022-02-01 08:06:27 -0800

    middle of background changer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Nav from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link, Outlet, useParams } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import './css/app.scss'
 import { TopPage } from './components/TopPage';
 import { About } from './components/About';
 import ContactMe from './components/ContactMe';
 
-export default function App() {
+const backgroundChanger = (location ) => {
+    let backgroundPic 
+    switch (location.pathname) {
+      case '/':
+        backgroundPic = 'app-con-background-home'
+        console.log(backgroundPic)
+        break;
+      case '/about':
+        backgroundPic = 'app-con-background-about'
+        console.log(backgroundPic)
+        break; 
+      case '/contact':
+        backgroundPic = 'app-con-background-contact'
+        console.log(backgroundPic)
+        break; 
+      default:
+        break;
+    }
+}
 
-  // const styleBackPic = `app-con-background-${}`
+export default function App() {
+  let location = useLocation();
+  
+
+  useEffect(()=>{
+    let backgroundClass = backgroundChanger(location)
+  },[location])
+
+  // useEffect(()=>{
+  //   let backgroundPic 
+  //   switch (location.pathname) {
+  //     case '/':
+  //       backgroundPic = 'app-con-background-home'
+  //       console.log(backgroundPic)
+  //       break;
+  //     case '/about':
+  //       backgroundPic = 'app-con-background-about'
+  //       console.log(backgroundPic)
+  //       break; 
+  //     case '/contact':
+  //       backgroundPic = 'app-con-background-contact'
+  //       console.log(backgroundPic)
+  //       break; 
+  //     default:
+  //       break;
+  //   }
+  // },[location])
 
   return (
     <>
-      <div className='app-con'>
+      <div className='app-con backgroundClass'>
 
         <Nav />
         <Routes>
